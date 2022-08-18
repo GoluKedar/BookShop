a3b0a192fe3940b01f250a7abab44bdfa3b5c1bf
Manish Kedar <manish.kedar@outlook.com>
2022-08-18 15:15:18 +0530

    Creating and Configurating View start and import

--- BookShop.Web/Areas/Customer/Controllers/HomeController.cs
+++ BookShop.Web/Areas/Customer/Controllers/HomeController.cs
@@ -7,8 +7,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BookShop.Web.Controllers
+namespace BookShop.Web.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
--- BookShop.Web/Startup.cs
+++ BookShop.Web/Startup.cs
@@ -63,7 +63,7 @@ namespace BookShop.Web
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
