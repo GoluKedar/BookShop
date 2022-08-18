e4f10a50ae78eb501f8d5ccc14f2cc840d33d9f1
Manish Kedar <manish.kedar@outlook.com>
2022-08-18 17:14:47 +0530

    Implemented Category Controller with operations

---
+++ BookShop.Web/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,80 @@
+﻿using BookShop.DataAccess.Repository.IRepository;
+using BookShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShop.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            Category category = new Category();
+
+            if (id == null)
+                return View(category);
+
+            category = _unitOfWork.Category.Get(id.GetValueOrDefault());
+
+            if (category == null)
+                return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                if (category.Id == 0)
+                {
+                    _unitOfWork.Category.Add(category);
+
+                }
+                else
+                {
+                    _unitOfWork.Category.Update(category);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+
+        #region API Call
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var categoryObj = _unitOfWork.Category.GetAll();
+            return Json(new { data = categoryObj });
+        }
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var categoryObj = _unitOfWork.Category.Get(id);
+            if (categoryObj == null)
+                return Json(new { success = false, message = "Error while deleting" });
+            _unitOfWork.Category.Remove(categoryObj);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Deleted successfully" });
+        }
+        #endregion
+    }
+}
